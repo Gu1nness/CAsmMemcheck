2e6c1840c183a6f3ce79b0573a726ebce7006ab9
Rémi Oudin <oudin@crans.org>
2019-01-16 17:21:26 +0100

    Example files

---
+++ examples/fibo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct Fibo {
+    int t1;
+    int t2;
+};
+
+int main() {
+    int i = 1, n=127, t1 = 0, t2 = 1, nextTerm;
+    struct Fibo fibo;
+
+    fibo.t1 = 0;
+    fibo.t2 = 1;
+
+    while(i<=n) {
+        nextTerm = fibo.t1 + fibo.t2;
+        fibo.t1 = fibo.t2;
+        fibo.t2 = nextTerm;
+        i++;
+    }
+    return fibo.t1;
+}
